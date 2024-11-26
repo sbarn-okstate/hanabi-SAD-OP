c61bb75514eacabe86d17b7e17c14643a88fd205
Sam <samuel.barney@okstate.edu>
2024-11-26 13:21:19 -0600

    pr

---
+++ SAD/pyhanabi/__init__.py
@@ -0,0 +1,7 @@
+import sys
+import os
+
+# Dynamically add the parent directory to sys.path
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+if parent_dir not in sys.path:
+    sys.path.append(parent_dir)
