da743a47e8a9adc6829cbbaa808f30ecf5e5baec
sbarn-okstate <samuel.barney@okstate.edu>
2024-12-09 17:25:54 -0600

    Update __init__.py

--- pyhanabi/__init__.py
+++ pyhanabi/__init__.py
@@ -2,7 +2,6 @@ from .common_utils import *
 import sys
 import os
 
-# Dynamically add the parent directory to sys.path
 parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 if parent_dir not in sys.path:
     sys.path.append(parent_dir)
