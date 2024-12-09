e594f24e078d1f701158f87b916d1ab538314853
sbarn-okstate <samuel.barney@okstate.edu>
2024-12-09 17:29:36 -0600

    Update set_path.py

--- pyhanabi/set_path.py
+++ pyhanabi/set_path.py
@@ -1,9 +1,6 @@
-# Copyright (c) Facebook, Inc. and its affiliates.
-# All rights reserved.
-#
-# This source code is licensed under the license found in the
-# LICENSE file in the root directory of this source tree.
-#
+"""
+Code from https://github.com/codeaudit/hanabi_SAD/blob/master/pyhanabi/set_p.py
+"""
 import os
 import sys
 
