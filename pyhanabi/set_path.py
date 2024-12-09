51e9726caf4d3244474635f445a02d1f180e0c77
sbarn-okstate <samuel.barney@okstate.edu>
2024-12-09 17:30:24 -0600

    Update set_path.py

--- pyhanabi/set_path.py
+++ pyhanabi/set_path.py
@@ -1,16 +1,1 @@
-"""
-Code from https://github.com/codeaudit/hanabi_SAD/blob/master/pyhanabi/set_p.py
-"""
-import os
-import sys
 
-
-def append_sys_path():
-    root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    tube = os.path.join(root, "build", "rela")
-    if tube not in sys.path:
-        sys.path.append(tube)
-
-    hanalearn = os.path.join(root, "build")
-    if hanalearn not in sys.path:
-        sys.path.append(hanalearn)
